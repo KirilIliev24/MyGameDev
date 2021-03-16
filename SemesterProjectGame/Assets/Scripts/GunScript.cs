227a012cb81569b6858718cefed52a5969b3349a
KirilIliev24 <43208740+KirilIliev24@users.noreply.github.com>
2021-03-16 17:00:28 +0200

    fire rate added

--- SemesterProjectGame/Assets/Scripts/GunScript.cs
+++ SemesterProjectGame/Assets/Scripts/GunScript.cs
@@ -7,23 +7,26 @@ public class GunScript : MonoBehaviour
     
     public float damage = 10f;
     float range = 100f;
-
+    public float fireRate = 15f;
     public Camera camera;
+
+    private float nextTimeToFire = 0f;
 
     public ParticleSystem muzzleFlash;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
         {
+            nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
         }
     }
 
     void Shoot()
     {
-        muzzleFlash.Play();
+        //muzzleFlash.Play();
         RaycastHit raycastHit;
 
         if (Physics.Raycast(camera.transform.position, camera.transform.forward, out raycastHit, range))
